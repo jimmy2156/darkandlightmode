18747b9721a996591ce84da53cb02104526ce0f9
Gaganjot singh <gaganjot25@gmail.com>
2022-07-31 14:39:25 +0530

    day 2

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -1,18 +1,17 @@
+import group1 from "../images/group.png"
 
-import group from "../images/reactjs.png"
 
-
-export default function Header() {
+export default function Header2() {
     return (
-        <div className="header">
-            <h1 className="heading3">Fun Facts about React</h1>
-            <ul>
-            <li className="heading44">Was first released in 2013</li>
+        <div className="header2">
+            <h1 className="heading4">Fun Facts about React</h1>
+            <ul className="ul2">
+            <li className="heading5">Was first released in 2013</li>
             <li className="heading5">Was originally created by Jordan Walke</li>
             <li className="heading6">Has well over 100K stars on Github</li>
             <li className="heading7">Is maintained by Facebook</li>
             <li className="heading8">Powers thousands of enterprise apps, including mobile apps</li></ul>
-            <img src={group}  className="logoimg" alt="" />
+            <img src={group1}  className="logoimg1" alt="" />
         </div>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 import logo from '../images/reactimg.svg'
+import Header from '../components/Header';
+import Header2 from '../components/Header2';
 
 
 export default function Navbar() {
+    let [main, setmain] = useState(<Header />)
+    function changeMode() {
+        setmain(prevState => prevState = <Header2 />)
+    }
+
+
     return (
-        <div className="navbar"><div className="h122">
+        <>
+        <div className="navbar">
+            
+            <div className="h122">
             <img src={logo} alt="react logo" className='img'/>
             <h1 className='heading1'>ReactFacts</h1></div>
             <div className="togglebutton">
                 <div>Light</div>
             <div className="toggle"><label className="switch">
-            <input type="checkbox" />
+            <input onClick={changeMode} type="checkbox" />
           <span className="slider round"></span></label> </div>
           <div>Dark</div>
                         
                         </div>
 
         </div>
+        {main}
+        </>
     )
 }
